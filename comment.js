ef7512701f1574540c9a8aacab5b84fcb30f3e62
Sergio Carabetta <scarabetta@gmail.com>
2023-08-04 15:30:41 +0000

    Again

--- comment.js
+++ comment.js
@@ -1,76 +1,19 @@
-// Create a web server
-// 1. GET /comments
-//    return all comments
-// 2. POST /comments
-//    create a new comment
-// 3. GET /comments/:id
-//    return a comment with given id
-// 4. PUT /comments/:id
-//    update a comment with given id
-// 5. DELETE /comments/:id
-//    delete a comment with given id
+// Create a web server that can respond to requests for /comments.json
+// with a list of comments in JSON format. Assume comments is an array
+// of comment objects, each with a "content" and a "date" property.
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var _ = require('underscore');
-var app = express();
-var PORT = 3000;
-
-var comments = [];
-var commentNextId = 1;
-
-app.use(bodyParser.json());
-
-app.get('/', function(req, res) {
-  res.send('Comment API Root');
+var http = require('http');
+var url = require('url');
+var comments = [{ content: "hello", date: "today" }];
+var server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.pathname === "/comments.json") {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(comments));
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("Not found");
+    }
 });
-
-// GET /comments
-app.get('/comments', function(req, res) {
-  res.json(comments);
-});
-
-// GET /comments/:id
-app.get('/comments/:id', function(req, res) {
-  var commentId = parseInt(req.params.id, 10);
-  var matchedComment = _.findWhere(comments, {id: commentId});
-
-  if (matchedComment) {
-    res.json(matchedComment);
-  } else {
-    res.status(404).send();
-  }
-});
-
-// POST /comments
-app.post('/comments', function(req, res) {
-  var body = _.pick(req.body, 'author', 'text');
-
-  if (!_.isString(body.author) || !_.isString(body.text) || body.author.trim().length === 0 || body.text.trim().length === 0) {
-    return res.status(400).send();
-  }
-
-  body.author = body.author.trim();
-  body.text = body.text.trim();
-
-  body.id = commentNextId++;
-  comments.push(body);
-  res.json(body);
-});
-
-// DELETE /comments/:id
-app.delete('/comments/:id', function(req, res) {
-  var commentId = parseInt(req.params.id, 10);
-  var matchedComment = _.findWhere(comments, {id: commentId});
-
-  if (!matchedComment) {
-    res.status(404).json({"error": "no comment found with that id"});
-  } else {
-    comments = _.without(comments, matchedComment);
-    res.json(matchedComment);
-  }
-});
-
-// PUT /comments/:id
-app.put('/comments/:id', function(req, res) {
-  var commentId = parseInt(req.params
+server.listen(8080);
