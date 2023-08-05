a6fba7cbbc6027b93439b44cd616e820fa2046a9
Sergio Carabetta <scarabetta@gmail.com>
2023-08-05 18:59:54 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server 
+
+var http = require('http');
+var url = require('url');
+var comments = [{ content: "hello", date: "today" }];
+var server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.pathname === "/comments.json") {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(comments));
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("Not found");
+    }
+});
+server.listen(8080);
